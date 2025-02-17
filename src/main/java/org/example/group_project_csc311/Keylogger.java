923d020fab17bf81ea1a6630a0fff1e1fa4aa6ec
Carlos Berio <carlos.a.berio@hotmail.com>
2025-02-17 12:00:42 -0500

    KeyLogger commit 2

---
+++ src/main/java/org/example/group_project_csc311/Keylogger.java
@@ -0,0 +1,55 @@
+package org.example.group_project_csc311;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keylogger implements KeyListener {
+
+
+    boolean upPressed, downPressed, leftPressed, rightPressed;
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if(code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if(code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if(code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+        if(code == KeyEvent.VK_S) {
+            downPressed = false;
+        }
+        if(code == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+        if(code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
+
+    }
+
+}
--- src/main/java/org/example/group_project_csc311/MazeApplication.java
+++ src/main/java/org/example/group_project_csc311/MazeApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MazeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Robot-View.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MazeApplication.class.getResource("robot-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
