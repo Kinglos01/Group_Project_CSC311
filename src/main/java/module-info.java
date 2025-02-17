974e970572439b526ea39d5ff0500cc5d8d2e3cc
Carlos Berio <carlos.a.berio@hotmail.com>
2025-02-17 10:54:42 -0500

    Init Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.example.group_project_csc311 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.group_project_csc311 to javafx.fxml;
+    exports org.example.group_project_csc311;
+}
