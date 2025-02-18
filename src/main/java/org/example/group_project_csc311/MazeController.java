aaf58ddd2c7e4d13dc8763037d09f7f289110421
Riiel-edu <riven18@farmingdale.edu>
2025-02-18 17:19:34 -0500

    Update MazeController.java
    commit and push, added the pixelColor() method, fixed some errors in the positionUpdate() method and added the shell for the autoplay method

--- src/main/java/org/example/group_project_csc311/MazeController.java
+++ src/main/java/org/example/group_project_csc311/MazeController.java
@@ -1,8 +1,12 @@
 package org.example.group_project_csc311;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import java.awt.event.KeyEvent;
+import javafx.scene.paint.Color;
 
 public class MazeController {
     
@@ -16,23 +20,64 @@ public class MazeController {
      * Updates the current position of the robot
      */
     @FXML
-    protected void positionUpdate() {
-
+    protected void positionUpdate(KeyEvent e) {
         KeyLogger keyLog = new KeyLogger();
 
-        int RobotSpeed = 10;
+        keyLog.keyPressed(e);
+
+        int robotSpeed = 10;
+
+        if(!(pixelColor((int) robot.getLayoutX(), (int) robot.getLayoutY(), keyLog))) {
+            if (keyLog.upPressed) {
+                robot.setLayoutY(robot.getLayoutY() - robotSpeed);
+            }
+            if (keyLog.downPressed) {
+                robot.setLayoutY(robot.getLayoutY() + robotSpeed);
+            }
+            if (keyLog.leftPressed) {
+                robot.setLayoutX(robot.getLayoutX() - robotSpeed);
+            }
+            if (keyLog.rightPressed) {
+                robot.setLayoutX(robot.getLayoutX() + robotSpeed);
+            }
+        }
+    }
+
+    /**
+     * Checks the pixel color
+     * @return true if the maze wall is in the way and false if it is not
+     */
+    @FXML
+    private boolean pixelColor(int x, int y, KeyLogger keyLog) {
+        Image mazeImage = maze.getImage();
+        PixelReader p = mazeImage.getPixelReader();
+        int posX = (x + 12);
+        int posY = (y + 12);
 
         if(keyLog.upPressed) {
-            robot.setY(robot.getY() + RobotSpeed);
+            posY -= 25;
         }
         if(keyLog.downPressed) {
-            robot.setY(robot.getY() - RobotSpeed);
+            posY += 25;
         }
         if(keyLog.leftPressed) {
-            robot.setX(robot.getX() - RobotSpeed);
+            posX -= 25;
         }
         if(keyLog.rightPressed) {
-            robot.setX(robot.getX() + RobotSpeed);
+            posX += 25;
         }
+
+        Color color = p.getColor(posX, posY);
+
+        return (color.toString()).equals("0x005399ff");
+
+    }
+
+    /**
+     * Autoplay the robot
+     */
+    @FXML
+    protected void autoplay() {
+
     }
 }
