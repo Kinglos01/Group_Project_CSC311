34f2bb05ce14fece2964d72057b38f4035b64649
Riiel-edu <riven18@farmingdale.edu>
2025-02-18 17:17:01 -0500

    commit and push, added the pixelColor() method, fixed some errors in the positionUpdate() method and added the shell for the autoplay method

--- src/main/java/org/example/group_project_csc311/MazeApplication.java
+++ src/main/java/org/example/group_project_csc311/MazeApplication.java
@@ -3,19 +3,27 @@ package org.example.group_project_csc311;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-
+import javafx.event.EventHandler;
 import java.io.IOException;
 
 public class MazeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MazeApplication.class.getResource("robot-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 425 );
+        Scene scene = new Scene(fxmlLoader.load(), 600, 475 );
         stage.setTitle("Maze");
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent keyEvent) {
+                System.out.println(keyEvent.getCode());
+            }
+        });
+
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
