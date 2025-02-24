c09a393c0d90d85d6fa0a3a37dd74b36740de32d
jabaltariq <21aidanrodriguez1@gmail.com>
2025-02-24 14:42:16 -0500

    finishing touches (again)

--- src/main/java/org/example/group_project_csc311/AutosolveHandler.java
+++ src/main/java/org/example/group_project_csc311/AutosolveHandler.java
@@ -78,78 +78,91 @@ public class AutosolveHandler {
             switch (step) {
                 case 0:
                     image.setLayoutX(image.getLayoutX() + 2);
+                    image.setRotate(0);
                     if (image.getLayoutX() > 40) {
                         step++;
                     }
                     break;
                 case 1:
                     image.setLayoutY(image.getLayoutY() - 2);
+                    image.setRotate(270);
                     if (image.getLayoutY() < 150) {
                         step++;
                     }
                     break;
                 case 2:
                     image.setLayoutX(image.getLayoutX() + 2);
+                    image.setRotate(0);
                     if (image.getLayoutX() > 260) {
                         step++;
                     }
                     break;
                 case 3:
                     image.setLayoutY(image.getLayoutY() - 2);
+                    image.setRotate(270);
                     if (image.getLayoutY() < 95) {
                         step++;
                     }
                     break;
                 case 4:
                     image.setLayoutX(image.getLayoutX() + 2);
+                    image.setRotate(0);
                     if (image.getLayoutX() > 320) {
                         step++;
                     }
                     break;
                 case 5:
                     image.setLayoutY(image.getLayoutY() + 2);
+                    image.setRotate(90);
                     if (image.getLayoutY() > 315) {
                         step++;
                     }
                     break;
                 case 6:
                     image.setLayoutX(image.getLayoutX() + 2);
+                    image.setRotate(0);
                     if (image.getLayoutX() > 370) {
                         step++;
                     }
                     break;
                 case 7:
                     image.setLayoutY(image.getLayoutY() - 2);
+                    image.setRotate(270);
                     if (image.getLayoutY() < 210) {
                         step++;
                     }
                     break;
                 case 8:
                     image.setLayoutX(image.getLayoutX() + 2);
+                    image.setRotate(0);
                     if (image.getLayoutX() > 480) {
                         step++;
                     }
                     break;
                 case 9:
                     image.setLayoutY(image.getLayoutY() - 2);
+                    image.setRotate(270);
                     if (image.getLayoutY() < 100) {
                         step++;
                     }
                     break;
                 case 10:
                     image.setLayoutX(image.getLayoutX() + 2);
+                    image.setRotate(0);
                     if (image.getLayoutX() > 540) {
                         step++;
                     }
                     break;
                 case 11:
                     image.setLayoutY(image.getLayoutY() + 2);
+                    image.setRotate(90);
                     if (image.getLayoutY() > 240) {
                         step++;
                     }
                     break;
                 case 12:
                     image.setLayoutX(image.getLayoutX() + 2);
+                    image.setRotate(0);
                     if (image.getLayoutX() > 570) {
                         step++;
                     }
